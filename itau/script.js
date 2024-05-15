481cc1e8d9f59b86e758cd9a9dc0e83e0f01ae18
ebagabe <gabriel.ebagabe@gmail.com>
2024-05-14 21:41:54 -0300

    Ended Project

--- itau/script.js
+++ itau/script.js
@@ -1,1 +1,27 @@
-console.log('teste')
+const eyes = document.querySelector('.container__info img')
+const balance = document.querySelector('.balance--ocult')
+const limit = document.querySelector('.limit--arrow span')
+
+let eyesActive = 0
+
+eyes.addEventListener('click', () => {
+    eyesActive++
+
+    if (eyesActive > 1) {
+        eyesActive = 0
+    }
+
+    if (eyesActive == 1) {
+        eyes.src = './img/open.png'
+        balance.textContent = 'R$ 100,00'
+        limit.textContent = 'R$ 100,00'
+        limit.style.fontSize = '16px'
+    }
+
+    if (eyesActive == 0) {
+        eyes.src = './img/closed.png'
+        balance.textContent = '....'
+        limit.textContent = '....'
+        limit.style.fontSize = '24px'
+    }
+})
