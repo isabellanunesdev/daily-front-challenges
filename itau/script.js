8bc0daf747270992a69e3c86080d8c9d7756b87f
Jeu14 <jedissonsantana34@gmail.com>
2024-05-18 13:47:12 -0300

    bug: fixed size of eye images

--- itau/script.js
+++ itau/script.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const visibilityButton = document.querySelector('.visibility-button');
+    const visibilityImg = visibilityButton.querySelector('img')
     const balanceImage = document.querySelector('.balance-img');
     const limitImage = document.querySelector('.limit-img');
 
     const balanceValue = 'R$ 1500,00';
     const limitValue = 'R$ 100,00';
-    const openEyeImg = '<img src="img/open.png" alt="limit hiden image" class="visibility-button">'
+
+    const openEyeSrc = "img/open.png";
+    const closedEyeSrc = "img/closed.png";
 
     const balanceHidenImg = '<img src="img/more-black.png" alt="balance hiden image">';
     const limitHidenImg = '<img src="img/more-gray.png" alt="limit hiden image">'
-    const closedEyeImg = '<img src="img/closed.png" alt="limit hiden image" class="visibility-button">'
 
 
 
     visibilityButton.addEventListener('click', function() {
         if (balanceImage.innerHTML === balanceValue) {
-            visibilityButton.innerHTML = closedEyeImg;
+            visibilityImg.src = closedEyeSrc;
             balanceImage.innerHTML = balanceHidenImg;
             limitImage.innerHTML = limitHidenImg;
         } else {
-            visibilityButton.innerHTML = openEyeImg
+            visibilityImg.src = openEyeSrc
             balanceImage.textContent = balanceValue;
             limitImage.textContent = limitValue;
         }
